1f26134abfc5a138b7ea429551a288f087669304
al.mixevvv <al.mixev@gmail.com>
2020-11-23 12:19:44 +0700

    Initial Commit
    Initial commit of the repo

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>weight</h1>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+    </div>
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string server = "192.168.1.3";
+            int port = 55555;
+
+            //open tcp connection to the telnet server
+            TcpClient client = new TcpClient(server, port);
+            NetworkStream stream = client.GetStream();
+
+            //prepare to receive the data
+            string responseData = string.Empty;
+            byte[] data = new byte[4096];
+
+            //read the stream
+            int bytes = stream.Read(data, 0, 100);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+            //show to UI
+            Label1.Text = responseData;
+
+            //close everything
+            stream.Close();
+            client.Close();
+
+        }
+    }
+}
